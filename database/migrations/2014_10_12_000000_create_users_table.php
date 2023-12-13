3d354e2d2c5eab26b94e60d8c8312a3a0c506f74
jorge <j0rg3e93@gmail.com>
2023-12-13 19:03:15 +0100

    update: se modifica la tabla de create_users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('player_users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean("is_active")->default(true);
+            $table->enum('role', ["user", "admin", "super_admin"])->default('user');
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("update_at")->useCurrent()->useCurrentOnUpdate();
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('player_users');
     }
 };
