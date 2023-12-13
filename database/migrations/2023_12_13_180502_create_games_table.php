4e49cf85ab50ef6d4cb916a6bcd02eaf373f4473
jorge <j0rg3e93@gmail.com>
2023-12-13 19:09:08 +0100

    update: se actualiza las columnas de la tabla de games

--- database/migrations/2023_12_13_180502_create_games_table.php
+++ database/migrations/2023_12_13_180502_create_games_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->boolean("is_active")->default(true);
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("update_at")->useCurrent()->useCurrentOnUpdate();
         });
     }
 
