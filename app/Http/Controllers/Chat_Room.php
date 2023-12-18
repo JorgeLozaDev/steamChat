253419ab9692f9277c63abef79f2d4bf0a7b5cf1
Rumanito12 <ady_9697@yahoo.com>
2023-12-18 17:17:09 +0100

    ADD: se añade metodo post para crear nuevos mensajes sgun el id del usuario y el id de la sala.

--- app/Http/Controllers/Chat_Room.php
+++ app/Http/Controllers/Chat_Room.php
@@ -43,4 +43,53 @@ class chat_room extends Controller
             );
         }
     }
+    
+    public function newMessage(Request $request,$idRoom)
+    {
+        try {
+            $user = auth()->user();
+            //verifico si su cuenta es activa.
+            if ($user->is_active === 0) {
+                return response()->json(
+                    [
+                        'succes' => false,
+                        'message' => 'su cuenta ha sido borrada'
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+            //recoger info
+            $id_player_sender = $user->id;
+            $id_rom = $idRoom;
+            $message = $request->input('message');
+
+            //se guarda
+            $newMessage = ModelChat_Room::create(
+                [
+                    "id_player_sender" => $id_player_sender,
+                    "id_room" => $id_rom,
+                    "message" => $message
+                ],
+            );
+
+            //respuesta
+                return response()->json(
+                    [
+                        'success' => true,
+                        'message' => 'GAME LIST',
+                        'data' => $newMessage
+                    ],
+                    Response::HTTP_OK
+                );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'succes' => false,
+                    'message' => 'NO',
+                    'error' => $th->getMessage()
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
