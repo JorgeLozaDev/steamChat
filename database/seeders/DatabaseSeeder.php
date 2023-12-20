2595280a316a03fa2c21e67a393fe4b94a10e2e1
jorge <j0rg3e93@gmail.com>
2023-12-20 09:17:25 +0100

    UPDATE: Se agrega un seed para crear un admin y poder usarlo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,12 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        \App\Models\PlayerUser::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
         \App\Models\PlayerUser::factory(10)->create();
         \App\Models\Rooms_Table::factory(10)->create();
         \App\Models\Room_User::factory(10)->create();
